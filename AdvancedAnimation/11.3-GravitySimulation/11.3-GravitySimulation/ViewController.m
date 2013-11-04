d5e57eaf5c7a15d0b3b145af76667e4db85657ec
Long Vinh Nguyen <long.omni@gmail.com>
2013-11-04 21:34:37 +0700

    Advanced Animation: TimeBased Animate

--- AdvancedAnimation/11.3-GravitySimulation/11.3-GravitySimulation/ViewController.m
+++ AdvancedAnimation/11.3-GravitySimulation/11.3-GravitySimulation/ViewController.m
@@ -10,6 +10,8 @@
 #import "Crate.h"
 #import "chipmunk.h"
 
+#define SIMULATION_STEP (1/120.0)
+
 @interface ViewController ()
 
 @property (nonatomic, weak) IBOutlet UIView *containerView;
@@ -34,16 +36,26 @@
     self.space = cpSpaceNew();
     cpSpaceSetGravity(self.space, cpv(0, -GRAVITY));
     
-    // add a Crate
-    Crate *crate = [[Crate alloc] initWithFrame:CGRectMake(100, 0, 100, 100)];
-    [self.containerView addSubview:crate];
-    cpSpaceAddBody(self.space, crate.body);
-    cpSpaceAddShape(self.space, crate.shape);
+    // add wall around edge of view
+    [self addWallShapeWithStart:cpv(0, 0) end:cpv(300, 0)];
+    [self addWallShapeWithStart:cpv(300, 0) end:cpv(300, 320)];
+    [self addWallShapeWithStart:cpv(300, 320) end:cpv(0, 320)];
+    [self addWallShapeWithStart:cpv(0, 320) end:cpv(0, 0)];
+    
+    
+    
+    // add a Crates
+    [self addCrateWithFrame:CGRectMake(0, 0, 32, 32)];
+    [self addCrateWithFrame:CGRectMake(32, 0, 32, 32)];
+    [self addCrateWithFrame:CGRectMake(64, 0, 64, 64)];
+    [self addCrateWithFrame:CGRectMake(128, 0, 32, 32)];
+    [self addCrateWithFrame:CGRectMake(0, 32, 64, 64)];
     
     // start the timer
     self.lastStep = CACurrentMediaTime();
     self.timer = [CADisplayLink displayLinkWithTarget:self selector:@selector(step:)];
     [self.timer addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    
 }
 
 void updateShape(cpShape *shape, void *unused)
@@ -58,14 +70,37 @@ void updateShape(cpShape *shape, void *unused)
 - (void)step: (CADisplayLink *)timer
 {
     // calculate step duration
-    CFTimeInterval thisStep = CACurrentMediaTime();
-    CFTimeInterval stepDuration = thisStep - self.lastStep;
-    self.lastStep = thisStep;
+//    CFTimeInterval thisStep = CACurrentMediaTime();
+//    CFTimeInterval stepDuration = thisStep - self.lastStep;
+//    self.lastStep = thisStep;
     
-    // update physics
-    cpSpaceStep(self.space, stepDuration);
+    CFTimeInterval frameTime = CACurrentMediaTime();
+    
+    // update simulation
+    while (self.lastStep < frameTime) {
+        cpSpaceStep(self.space, SIMULATION_STEP);
+        self.lastStep += SIMULATION_STEP;
+    }
+    
     // update all the shape
     cpSpaceEachShape(self.space, &updateShape, NULL);
+}
+
+- (void)addCrateWithFrame:(CGRect)frame
+{
+    Crate *crate = [[Crate alloc] initWithFrame:frame];
+    [self.containerView addSubview:crate];
+    cpSpaceAddBody(self.space, crate.body);
+    cpSpaceAddShape(self.space, crate.shape);
+}
+
+- (void)addWallShapeWithStart:(cpVect)start end:(cpVect)end
+{
+    cpShape *wall = cpSegmentShapeNew(self.space->staticBody, start, end, 1);
+    cpShapeSetCollisionType(wall, 2);
+    cpShapeSetFriction(wall, 0.5);
+    cpShapeSetElasticity(wall, 0.8);
+    cpSpaceAddStaticShape(self.space, wall);
 }
 
 - (void)didReceiveMemoryWarning
