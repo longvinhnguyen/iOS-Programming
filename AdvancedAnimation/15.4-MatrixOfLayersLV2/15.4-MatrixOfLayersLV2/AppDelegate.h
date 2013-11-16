55093fdf4bd69da595d1731ad13dc3378251695e
Long Vinh Nguyen <long.omni@gmail.com>
2013-11-16 22:50:38 +0700

    Advanced Animation - MatrixOfLayersLV2

---
+++ AdvancedAnimation/15.4-MatrixOfLayersLV2/15.4-MatrixOfLayersLV2/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  15.4-MatrixOfLayersLV2
+//
+//  Created by Long Vinh Nguyen on 11/16/13.
+//  Copyright (c) 2013 Home Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
