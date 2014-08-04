232640d1797905ca94007b9b49c0c0ff1643b2ba
Long Vinh Nguyen <long.omni@gmail.com>
2014-08-04 22:58:48 +0700

    - Push to specified device

--- parsepush/parsepush/ViewController.m
+++ parsepush/parsepush/ViewController.m
@@ -5,6 +5,7 @@
 //  Created by Long Vinh Nguyen on 8/4/14.
 //  Copyright (c) 2014 Home Inc. All rights reserved.
 //
+#import <Parse/Parse.h>
 
 #import "ViewController.h"
 
@@ -24,6 +25,18 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)sendMessage:(id)sender
+{
+    PFQuery *pushQuery = [PFInstallation query];
+    if ([[UIDevice currentDevice].model rangeOfString:@"iPad"].length == 0) {
+        [pushQuery whereKey:@"deviceToken" equalTo:@"d0d86fd0cb6be314ca2c5dcf2885fc93e69c28ec8a1ea16fc96209c0a572ab7c"];
+    } else {
+        [pushQuery whereKey:@"deviceToken" equalTo:@"e7d86db3d69cc37da7d164e37f7ef6eebcb3f062ab42372e4d687c054a03d85c"];
+    }
+    [PFPush sendPushMessageToQuery:pushQuery withMessage:@"I know you were trouble when you walk in. Shame on me right now." error:nil];
+    
 }
 
 @end
